45cc2ac50c07f9ae0965bcd98b703bcd30e47001
Xin Zhou <maiziex@gmail.com>
2017-08-15 22:20:21 -0700

    Add files via upload

---
+++ doc/run_step1-2.sh
@@ -0,0 +1,5 @@
+# step 1:
+python ExtractXX_step1.py -i ../../S_24385_L1_barcoded_sorted_rmdup_bybarcode.bam -f1 ../../S_24385_L1.RA.fastq -f2 ../../S_24385_L1.I1.fastq -b /scratch/users/xzhou15/SimProj_10X/RealData/Scripts/barcode4M.fa --out_dir ../../extract_10x/ --h5_dir ../../qual_20/ --Cr 0.2 --flag all 
+
+
+
